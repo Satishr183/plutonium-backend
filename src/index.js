f48c8f3fad95b8b52159f93ee4e92d09ba27dbff
Satish <satish_95raj@outlook.com>
2022-08-19 13:27:16 +0530

    populate task

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Satishr183:6SdvyY7phCfH5VsV@cluster0.ewmx8.mongodb.net/Populate-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
