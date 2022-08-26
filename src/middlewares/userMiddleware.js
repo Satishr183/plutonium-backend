55349e17db9f263e45daabe91f6023018f4c99b7
Satish <satish_95raj@outlook.com>
2022-08-26 13:05:14 +0530

    middleware Assignment Done

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -3,7 +3,7 @@ const productModel = require("../models/productModel")
 const userModel = require("../models/userModel")
 
 const mid = function ( req, res, next) {
-let isFreeAppUser= req.headers.isfreeappuser
+let isFreeAppUser= (req.headers.isfreeappuser)
     
     if(!isFreeAppUser){
         res.send({status:false, msg:"request is missing !. Mandatory header"})
@@ -16,14 +16,13 @@ let isFreeAppUser= req.headers.isfreeappuser
 }
 
 const mid2= async function(req, res, next){
-    let isFreeAppUser=req.headers.isfreeappuser
+    let isFreeAppUser=(req.headers.isfreeappuser)
     let data =req.body
     let isproduct = await productModel.findOne({_id:data.productId})
     let isuser =await userModel.findOne({_id:data.userId});
    
-    if(isFreeAppUser=="true"){
-        const freeStatus = await userModel.findOneAndUpdate({_id:data.userId},{$set:{isFreeAppUser:true}},{new:true})
-        console.log(freeStatus)
+    if(isFreeAppUser=='true'){
+        data.isFreeAppUser=true
         next()
 
     }else if (isproduct['price']<isuser['balance']){
